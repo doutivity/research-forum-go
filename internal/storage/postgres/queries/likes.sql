2bc94c1c1e600a1982a519bf226d11698a8b4dcc
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2023-12-22 19:47:17 +0200

    #9 review

--- internal/storage/postgres/queries/likes.sql
+++ internal/storage/postgres/queries/likes.sql
@@ -1,14 +1,14 @@
 -- name: LikesUpsert :exec
 INSERT INTO likes (comment_id, active, created_at, created_by, updated_at, updated_by)
 VALUES (@comment_id, @active, @created_at, @created_by, @updated_at, @updated_by)
-ON CONFLICT (comment_id, created_by) 
-DO UPDATE SET 
-    active = EXCLUDED.active, 
-    updated_at = EXCLUDED.updated_at, 
-    updated_by = EXCLUDED.updated_by;
+ON CONFLICT (comment_id, created_by)
+    DO UPDATE SET active     = EXCLUDED.active,
+                  updated_at = EXCLUDED.updated_at,
+                  updated_by = EXCLUDED.updated_by;
 
 -- name: LikesByCommentIDs :many
-SELECT likes.comment_id, likes.created_at, users.*
+SELECT likes.comment_id, users.*
 FROM likes
-JOIN users ON likes.created_by = users.user_id
-WHERE likes.comment_id = ANY(@comment_ids::BIGINT[]) AND likes.active = TRUE;
+  JOIN users ON likes.created_by = users.user_id
+WHERE likes.comment_id = ANY (@comment_ids::BIGINT[])
+  AND likes.active = TRUE;
